c9bcdd17d4dcc26ffba0a8c082a96a67ca551877
Carlos Mayer <carlitos5525.cap@gmail.com>
2022-05-01 15:27:26 -0300

    area_responsavel done

--- responsavel/registrar_nova_matricula.js
+++ responsavel/registrar_nova_matricula.js
@@ -33,3 +33,39 @@ function inserirLinhaTabela(disciplina, professor) {
     alert("Matrícula realizada com sucesso!");
     
 }
+
+function matricular_ano_letivo(){
+    
+    serie_escolhida = document.getElementById('serie_escolhida');
+    var serie = serie_escolhida.options[serie_escolhida.selectedIndex].text;
+
+    turno_escolhido = document.getElementById('turno_escolhido');
+    var turno = turno_escolhido.options[turno_escolhido.selectedIndex].text;
+
+    inserirLinhaTabelaAnoLetivo(serie, turno);
+    
+}
+
+function inserirLinhaTabelaAnoLetivo(serie, turno){
+    // Captura a referência da tabela com id “minhaTabela”
+    var table = document.getElementById("tabela_ano_letivo");
+    // Captura a quantidade de linhas já existentes na tabela
+    var quantd_linhas = table.rows.length;
+    // Insere uma linha no fim da tabela.
+    var nova_linha = table.insertRow(quantd_linhas);
+
+    coluna_ano = nova_linha.insertCell(0);
+    coluna_aluno = nova_linha.insertCell(1);
+    coluna_turno = nova_linha.insertCell(2);
+    coluna_serie = nova_linha.insertCell(3);
+
+    coluna_ano.innerHTML = '2022';
+    coluna_aluno.innerHTML = 'Aluno do Responsável';
+    coluna_turno.innerHTML = turno;
+    coluna_serie.innerHTML = serie;
+
+    document.getElementById("botao_fechar").click();
+    
+
+    alert("Matrícula realizada com sucesso!");
+}
