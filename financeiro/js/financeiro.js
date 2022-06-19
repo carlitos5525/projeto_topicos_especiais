48f3cd293c500d6f54bb6744892a0b640a2ad8d2
Carlos Mayer <carlitos5525.cap@gmail.com>
2022-06-19 15:46:51 -0300

    add financeiro

---
+++ financeiro/js/financeiro.js
@@ -0,0 +1,43 @@
+function base_dados(id_aluno){
+    var requestURL = '/base_dados/matricula_atividade_extra.json';
+
+    localStorage.setItem("id_aluno_financeiro", id_aluno);
+
+    var request = new XMLHttpRequest();
+
+    request.open('GET', requestURL);
+
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function(){
+        var matriculas_atividades = request.response;
+        populate_matriculas_atividades(matriculas_atividades);
+    }
+}
+
+function populate_matriculas_atividades(jsonOBJ){
+    var matriculas_atividades = jsonOBJ['matriculas_atividades'];
+
+    //salvando o aluno a ser procurado
+    var id_aluno = localStorage.getItem("id_aluno_financeiro");
+    
+    var matri_atividades_do_aluno = [];
+    
+    //filtrando apenas pelas atividades do aluno
+    for(index in matriculas_atividades){
+      if(matriculas_atividades[index].aluno_id == id_aluno){
+           matri_atividades_do_aluno.push(matriculas_atividades[index]);
+        }
+    }
+    
+    t_body = document.getElementById('t_body');
+
+    t_body.innerHTML = '';
+    for(index in matri_atividades_do_aluno){
+        matricula_da_atv = "<tr>" + "<td>" + matri_atividades_do_aluno[index].nome_aluno + " </td>" + 
+        "<td>" + matri_atividades_do_aluno[index].nome_atividade + " </td>" + 
+         "</tr>";
+        t_body.insertAdjacentHTML('beforeend', matricula_da_atv);
+    }
+}
